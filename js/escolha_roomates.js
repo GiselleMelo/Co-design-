103fac68ab48cd271d32d5236b8566be75e85c09
André Corrêa Santos <54818184+AndreCorreaSantos@users.noreply.github.com>
2021-11-17 14:45:41 -0300

    Adicionando botoes nas imagens da lista de escolha

---
+++ js/escolha_roomates.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function(){
+    let img = document.querySelector(".profile")
+    img.addEventListener("click",function(event){
+        window.location.href = "chat.html";
+    });
+ })
